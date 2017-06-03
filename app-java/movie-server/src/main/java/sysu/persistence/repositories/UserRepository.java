54f1ecb6725de3d2575080b4ec39cb903b285e3e
LZhiYue <q2912982@hotmail.com>
2017-06-03 09:38:33 +0800

    nothing change

--- app-java/movie-server/src/main/java/sysu/persistence/repositories/UserRepository.java
+++ app-java/movie-server/src/main/java/sysu/persistence/repositories/UserRepository.java
@@ -1,7 +1,11 @@
 package sysu.persistence.repositories;
 
+import org.springframework.data.repository.CrudRepository;
+import sysu.persistence.models.User;
+
 /**
  * Created by Shower on 2017/5/10 0010.
  */
-public class UserRepository {
+public interface UserRepository extends CrudRepository<User, Long> {
+
 }
