fd5cfcadb8dea12ae47af7693088e902436ae156
Lazy coder <1413174416@qq.com>
2017-06-04 13:57:48 +0800

    finished

--- app-java/movie-server/src/main/java/sysu/MovieServerApplication.java
+++ app-java/movie-server/src/main/java/sysu/MovieServerApplication.java
@@ -70,6 +70,9 @@ public class MovieServerApplication {
 				hallRepository.save(s.getHall());
 				movieRepository.save(s.getMovie());
 				screeningRepository.save(s);
+				for (int i = 1; i < 5; i++)
+					for (int j = 1; j < 5; j++)
+						seatRepository.save(new Seat(i, j, s));
 			}
 
 
@@ -96,8 +99,6 @@ public class MovieServerApplication {
 	public JdkMongoSessionConverter jdkMongoSessionConverter() {
 		return new JdkMongoSessionConverter();
 	}
-
-
 }
 
 class generator {
